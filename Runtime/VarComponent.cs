6db37c81e958f3992c433ec1719993f3532559ac
SandroGX <sandroalemflor@hotmail.com>
2019-09-21 22:13:25 +0100

    Package created

---
+++ Runtime/VarComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GX.VarSystem
+{
+    public class VarComponent : Variable
+    {
+        public System.Type compType;
+
+        public override Variable Duplicate(object varHolder)
+        {
+            if (isStatic)
+            {
+                if (va == null)
+
+                    return this;
+            }
+
+            VarComponent v = CreateInstance<VarComponent>();
+            v.va = ((Component)varHolder).GetComponent(compType);
+            return v;
+        }
+    }
+}
